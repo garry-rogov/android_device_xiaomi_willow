e4bbec604929fe6d839303a21dd064f57ab44750
garry-rogov <garry.rogov@gmail.com>
2024-03-19 12:52:13 +0300

    ginkgo/willow: willow: Drop Translations overlay

--- device.mk
+++ device.mk
@@ -13,12 +13,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Overlays - RU translations
-DEVICE_PACKAGE_OVERLAYS += \
-    packages/resources/translations/overlay
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    packages/resources/translations/overlay
-
 PRODUCT_PACKAGES += \
     GinkgoMiuiCamera \
     GinkgoNoCutoutOverlay \
